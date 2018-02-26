f976389dfe9d6b7c42bec8151b8ca849fa7cb31d
linberon <linberon@gmail.com>
2018-02-26 13:18:46 +0000

    frst commit

--- app.js
+++ app.js
@@ -1,21 +1,64 @@
-var newEl = document.createElement('li');
-var newText = document.createTextNode('tomato');
-newEl.appendChild(newText);
-var deleteButton = document.createElement('button'); // create button
-deleteButton.textContent = 'delete'; // label it delete
-$(deleteButton).on('click', function() { newEl.remove(); }); // click event handler which calls remove on the li element
-newEl.appendChild(deleteButton); // add to list item
-var position = document.getElementsByTagName('ul')[0];
-position.appendChild(newEl);
+$(function() {
+  //Create a state object, with a todoItems array.
+  state = {
+    toDoItems: []
+  };
 
-var nextId = 0;
-// var newEl = …
-newEl.id = nextId++
-// ...
-$(deleteButton).on('click', function() { $('#' + nextId).remove(); });
+  //Rendering the data. renderElement(item) returns HTML for ONE element in the todoItems array
+
+  function renderElement(item, index) {
+      return `<li>
+                    <span class="shopping-item">${item.name}</span>
+                <div class="shopping-item-controls">
+                  <button data-index="${item.index}" class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                    </button>
+                    <button data-index="${item.index}" class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                  </button>
+                </div>
+              </li>`;
+  }
+
+  // use renderElement with state.todoItems.map to generate HTML for
+  // all elements in todoItems
+
+  function renderAllElements() {
+    var elementsHTML = state.toDoItems.map(renderElement);
+    $(".shopping-list").html(elementsHTML);
+  }
+
+  // write the addItem(name) function to add the new element into the todoItems array.
+
+  function addItem(name) {
+    state.toDoItems.push({ name: name, checked: false });
+    renderAllElements();
+  }
+
+  // Add an event listener for the js-shopping-list-form
+
+  $("#js-shopping-list-form").submit(function(event) {
+    event.preventDefault();
+    var name = $("#shopping-list-entry").val();
+    addItem(name);
+  });
+
+  // Add an event listener for the check buttons.
+ 
+  $(".shopping-list").on("click", ".shopping-item-delete", function() {
+    var index = $(this).attr("data-index");
+    state.toDoItems.splice(index, 1);
+    renderAllElements();
+  });
+
+  renderAllElements();
+});
 
 
-var checkButton = document.createElement('button'); // create button
-checkButton.textContent = 'check'; //label it check
-$(checkButton).on('click', function(){newEl.addClass('shopping-item__checked'); });
-newEl.appendChild(checkButton);
+ $(".shopping-list").on("click", ".shopping-item-toggle", function() {
+    var itemIndex = $(event.currentTarget).find("li").attr("data-index");
+    $(".shopping-item").attr("data-index", itemIndex).toggleClass("shopping-item__checked");
+    renderAllElements();
+  });
+
+  renderAllElements();
